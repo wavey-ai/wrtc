68594d8b038ece11a02fdad5bb03c710fb0ab2ce
jbrough <jamie1612@gmail.com>
2024-12-18 02:55:16 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 #[cfg(not(target_arch = "wasm32"))]
+pub mod client;
+
+#[cfg(not(target_arch = "wasm32"))]
 pub mod server;
 
+#[cfg(not(target_arch = "wasm32"))]
+mod topology;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod state;
+
+#[cfg(target_arch = "wasm32")]
 pub mod wasm;
